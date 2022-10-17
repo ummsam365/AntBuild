288c78fc7920019ebbfc93a3a657b452793d670c
User -2022XOGZC\Administrator <psy7758@hanmail.net>
2022-10-17 12:53:40 +0900

    Test01-이클립스 commit Test

---
+++ src/com/ncs/green/service/VOService.java
@@ -0,0 +1,17 @@
+package com.ncs.green.service;
+
+import com.ncs.green.vo.MemberVO;
+
+public class VOService {
+	
+	private MemberVO memberVO;
+
+	public MemberVO getMemberVO() {
+		return memberVO;
+	}
+
+	public void setMemberVO(MemberVO memberVO) {
+		this.memberVO = memberVO;
+	}
+}
+ //class
